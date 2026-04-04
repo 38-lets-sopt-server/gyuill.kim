b89ca5feea358d2f906ccbbd6cc3d56456a6fc5c
Kimgyuilli <rlarbdlf222@naver.com>
2026-04-04 16:00:09 +0900

    feat: add post dto

---
+++ src/main/java/org/sopt/PostResponse.java
@@ -0,0 +1,24 @@
+package org.sopt;
+
+import java.time.LocalDateTime;
+
+class PostResponse {
+    Long id;
+    String title;
+    String content;
+    String author;
+    LocalDateTime createdAt;
+
+    public PostResponse(Post post) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.author = post.getAuthor();
+        this.createdAt = post.getCreatedAt();
+    }
+
+    @Override
+    public String toString() {
+        return "[" + id + "] " + title + " - " + author + " (" + createdAt + ")\n" + content;
+    }
+}
